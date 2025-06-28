82c7f3d6f3789712ab22f0e91fc73feac9e33652
venturas55 <venturas55@hotmail.com>
2025-06-28 11:11:23 +0200

    a ver ahora... handlebars

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -8,8 +8,8 @@ helpers.timeago = (timestamp) => {
 helpers.formatearSp = (fecha) => {
   if (fecha != null) {
     var timestamp = new Date(fecha);
-    mnth = ("0" + (timestamp.getMonth() + 1)).slice(-2),
-      day = ("0" + timestamp.getDate()).slice(-2);
+    const mnth = ("0" + (timestamp.getMonth() + 1)).slice(-2),
+    day = ("0" + timestamp.getDate()).slice(-2);
     return [day, mnth, timestamp.getFullYear()].join("/");
   } else {
     return '';
@@ -19,7 +19,7 @@ helpers.formatearSp = (fecha) => {
 helpers.formatearSpHora = (fecha) => {
   if (fecha != null) {
     var timestamp = new Date(fecha);
-    mnth = ("0" + (timestamp.getMonth() + 1)).slice(-2),
+    const mnth = ("0" + (timestamp.getMonth() + 1)).slice(-2),
       day = ("0" + timestamp.getDate()).slice(-2),
       hours = ("0" + timestamp.getHours()).slice(-2),
       minutes = ("0" + timestamp.getMinutes()).slice(-2),
@@ -33,7 +33,7 @@ helpers.formatearSpHora = (fecha) => {
 helpers.formatearHoraSp = (fecha) => {
   if (fecha != null) {
     var timestamp = new Date(fecha);
-    mnth = ("0" + (timestamp.getMonth() + 1)).slice(-2),
+    const mnth = ("0" + (timestamp.getMonth() + 1)).slice(-2),
       day = ("0" + timestamp.getDate()).slice(-2),
       hours = ("0" + timestamp.getHours()).slice(-2),
       minutes = ("0" + timestamp.getMinutes()).slice(-2),
@@ -46,14 +46,14 @@ helpers.formatearHoraSp = (fecha) => {
 //Este es el formateo necesario para encajar una fecha en un input de type="date"
 helpers.formatearEn = (fecha) => {
   var timestamp = new Date(fecha);
-  mnth = ("0" + (timestamp.getMonth() + 1)).slice(-2),
+  const mnth = ("0" + (timestamp.getMonth() + 1)).slice(-2),
     day = ("0" + timestamp.getDate()).slice(-2);
   return [timestamp.getFullYear(), mnth, day].join("-");
 }
 
 helpers.tiempoHasta = (fecha) => {
   let dias, horas, min, sec;
-  var diff = fecha - new Date();
+  const diff = fecha - new Date();
   dias = (diff / 60000 / 60 / 24);
   horas = (dias - Math.trunc(dias))*24;
   min = (horas-Math.trunc(horas))*60;
