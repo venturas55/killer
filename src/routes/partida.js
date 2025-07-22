7c4a907249a007a0fce36d30bae2d1c7c772a73f
venturas55 <venturas55@hotmail.com>
2025-07-23 01:10:03 +0200

    Se evita que un usuario pueda confirmar su muerte si tiene un ticket pendiente.

--- src/routes/partida.js
+++ src/routes/partida.js
@@ -469,7 +469,7 @@ router.get("/:id_partida/asesinar/:id_victima", funciones.isAuthenticated, async
     await db.query("update partidasenjuego set ticket = true where id_partida=? AND id_jugador=? AND id_victima=?", [id_partida, req.user.id, id_victima])
     const [destinatario] = await db.query("select * from usuarios where id=?", id_victima);
     enviarCorreo(req, res, destinatario, id_partida);
-    funciones.writeLog("PARTIDA: "+id_partida+ " ==> "+ req.user.id + " notifica asesinato de " + id_victima);
+    funciones.writeLog("PARTIDA: " + id_partida + " ==> " + req.user.id + " notifica asesinato de " + id_victima);
     req.flash("success", "Aviso de asesinato enviado");
     res.redirect("/partidas/plantilla/" + id_partida);
   } catch (error) {
@@ -488,7 +488,7 @@ router.get("/:id_partida/borrarasesinar/:id_victima", funciones.isAuthenticated,
 
   try {
     await db.query("update partidasenjuego set ticket = false where id_partida=? AND id_jugador=? AND id_victima=?", [id_partida, req.user.id, id_victima])
-    funciones.writeLog("PARTIDA: "+id_partida+ " ==> "+ req.user.id + " cancela notificacion de asesinato de " + id_victima);
+    funciones.writeLog("PARTIDA: " + id_partida + " ==> " + req.user.id + " cancela notificacion de asesinato de " + id_victima);
     req.flash("success", "Aviso de asesinato borrado");
     res.redirect("/partidas/plantilla/" + id_partida);
   } catch (error) {
@@ -527,11 +527,17 @@ router.get("/:id_partida/muerte/:id_jugador?", async (req, res) => {
     //TODO: si algun otro jugador tiene un ticket de que le ha matado este jugador
     //No se puede confirmar muerte si otro Jugador tiene una muerte pendiente por confirmar de éste.
     //QUE HACER CON EL TICKET?? Ahora mismo creo que no pasaria nada,Se borraria el ticket y se asinaria al siguiente.
-    /*     let [ticket] = await db.query(
-          "SELECT * FROM partidasenjuego WHERE id_jugador=? AND id_partida=? AND ticket=1" ,
-          [id_jugador, id_partida]
-        ); */
-    //FIN
+    const ticket = await db.query(
+      "SELECT * FROM partidasenjuego WHERE id_jugador=? AND id_partida=? AND ticket=1",
+      [id_jugador, id_partida]
+    );
+    console.log("ticket",ticket);
+    console.log("ticket",ticket.length);
+    if (ticket.length > 0) {
+      req.flash("danger", "No puedes aceptar tu muerte. Otro jugador tiene una muerte pendiente en la que tú eres el asesino. Cancela ese ticket o espera a que se resuelva.");
+      return res.redirect(isLoggedIn ? `/partidas/plantilla/${id_partida}` : "/error");
+    }
+    //FIN //TODO:
 
     let [asesino] = await db.query(
       "SELECT * FROM partidasenjuego WHERE id_victima=? AND id_partida=?",
@@ -591,7 +597,7 @@ router.get("/:id_partida/muerte/:id_jugador?", async (req, res) => {
       "SELECT * FROM partidasenjuego WHERE eliminado=0 AND id_partida=?",
       [id_partida]
     );
-    funciones.writeLog("PARTIDA: "+id_partida+ " ==> "+ id_jugador + " acepta que fue asesinado por " + asesino.id_jugador);
+    funciones.writeLog("PARTIDA: " + id_partida + " ==> " + id_jugador + " acepta que fue asesinado por " + asesino.id_jugador);
     if (supervivientes.length === 1) {
       await db.query("UPDATE partidas SET status='finalizada' WHERE id=?", [id_partida]);
       if (supervivientes[0].id_jugador === id_jugador) {
